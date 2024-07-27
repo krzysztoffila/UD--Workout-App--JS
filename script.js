3ac1a95e93fbce4d1b3a70b674f46f1507a6f0ef
krzysztoffila <umowy.fila@gmail.com>
2024-07-27 21:47:17 +0200

    Add pin on click

--- script.js
+++ script.js
@@ -21,11 +21,14 @@ if (navigator.geolocation)
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-
       L.marker(coords)
         .addTo(map)
         .bindPopup('A pretty CSS popup.<br> Easily customizable.')
         .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng]).addTo(map).bindPopup('New').openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
